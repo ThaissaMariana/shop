567bd17194745c2d82fd3462f2a9b41d566e7836
ThaissaMariana <thaissa2608mariana@gmail.com>
2024-05-26 01:43:55 -0300

    Commit alt

--- src/app/components/Footer.js
+++ src/app/components/Footer.js
@@ -1,1 +1,10 @@
-import React from "react"
+import React from "react";
+import styles from "./footer.module.css";
+
+export default function Footer() {
+    return (
+        <footer className={styles.footer}>
+            <p>alguma coisa</p>
+        </footer>
+    );
+}
