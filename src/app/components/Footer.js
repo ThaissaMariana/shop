d3f2c272969a5250765090532ec9168d0144d82f
ThaissaMariana <thaissa2608mariana@gmail.com>
2024-06-01 04:52:42 -0300

    Footer, footer.css e header.css
    No Footer foi importado a imagem e colocado o link, e foi criado a lista não-ordenada com as 4 plataformas escolhidas e por último foi colocado a data criada da fundação da empresa. No footer.css foi trocado a cor de fundo do rodapé e adicionado algumas coisas (cor, fonte e centralização) que precisavam, também foi colocado o tamanho do logo e algumas coisas bastante necessárias para o funcionamento da lista com as redes. E adicionalmente também foi trocado a escrita da cor do header.

--- src/app/components/Footer.js
+++ src/app/components/Footer.js
@@ -1,10 +1,23 @@
 import React from "react";
 import styles from "./footer.module.css";
+import Image from "next/image";
 
 export default function Footer() {
     return (
         <footer className={styles.footer}>
-            <p>alguma coisa</p>
+        <div>
+        <Image className={styles.logo}
+                width={100}
+                height={80}
+                src={"https://i.pinimg.com/originals/6e/6d/88/6e6d881c989837d9c959886b88588357.png"} />
+        </div>
+        <ul className={styles.redes}>
+            <li><a href="#">Instagram</a></li>
+            <li><a href="#">Telegram</a></li>
+            <li><a href="#">TikTok</a></li>
+            <li><a href="#">Twitter</a></li>
+        </ul>
+        <p>Empresa fundada em: 24 de agosto de 2006</p>
         </footer>
     );
 }
