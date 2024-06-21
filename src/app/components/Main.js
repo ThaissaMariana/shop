f79917d3cabfdbfd0db44ad894706c218fc04d3f
ThaissaMariana <thaissa2608mariana@gmail.com>
2024-06-21 16:04:30 -0400

    Commit - Aula 21/06

--- src/app/components/Main.js
+++ src/app/components/Main.js
@@ -6,13 +6,15 @@ import Spinner from "./Spinner";
 
 export default function Main() {
     const [listProduct, setListProduct] = useState([]);
+    const [listComplete, setListComplete] = useState ([]);
+    const [search, setSearch] = useState ("");
 
     useEffect( ()=> {
         const getProduct = async () =>{
             const response = await fetch("https://fakestoreapi.com/products");
             const data = await response.json(); 
-
             setListProduct(data);
+            setListComplete(data);
         }
         getProduct();
     }, []);
@@ -47,6 +49,18 @@ export default function Main() {
     setListProduct(newList);
    }
 
+   const searchText = (text) => {
+      setSearch(text);
+
+      if( text.trim() == ""){
+            setListProduct(listComplete);
+            return
+      }
+      const newList = listProduct.filter((product) =>
+         product.title.toUpperCase().trim().includes(search.toUpperCase().trim()))
+      setListProduct(newList);
+   }
+
    if(listProduct[0] == null){
     return <Spinner/>
    }
@@ -54,6 +68,10 @@ export default function Main() {
     return(
         <>
         <div className={styles.fundo}>
+           <input type="text" value={search} placeholder="Pesquise o produto"
+            onChange={(event) => searchText( event.target.value )}/>
+
+            <button className={styles.buttonn} onClick={orderZa}>Za</button>
             <button className={styles.button} onClick={orderAz}>AZ</button>
             <button className={styles.button} onClick={orderZa}>ZA</button>
             <button className={styles.button} onClick={ordermais}>Menor Preço</button>
